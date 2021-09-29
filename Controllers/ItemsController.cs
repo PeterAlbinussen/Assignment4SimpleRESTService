8e3af88a042fcf7473922d3baf074bcbd0012cbd
Peter Albinussen <pete47m0@edu.easj.dk>
2021-09-29 11:10:52 +0200

    Add project files.

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment4SimpleRESTService.Managers;
+using Assignment4SimpleRESTService.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Assignment4SimpleRESTService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly ItemsManager _manager = new ItemsManager();
+
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Item> Get()
+        {
+            return _manager.GetAll();
+        }
+
+
+        // GET api/<ItemsContrBookr>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ItemsController>
+        [HttpPost]
+        public Item Post([FromBody] Item value)
+        {
+            return _manager.Add(value);
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public Item Put(int id, [FromBody] Item value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        // DELETE api/<ItemsController>/5
+        [HttpDelete("{id}")]
+        public Item Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ Managers/ItemsManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment4SimpleRESTService.Models;
+
+namespace Assignment4SimpleRESTService.Managers
+{
+    public class ItemsManager
+    {
+        private static int _nextId = 1;
+        private static readonly List<Item> Data = new List<Item>
+        {
+            new Item {Id = _nextId++, Name = "Soda", ItemQuality = 12, Quantity = 3},
+            new Item {Id=_nextId++, Name = "Milk", ItemQuality = 4, Quantity = 4},
+            new Item {Id=_nextId++, Name = "Slimfit pants", ItemQuality = 7, Quantity = 6},
+            new Item {Id=_nextId++, Name = "Calculator", ItemQuality = 2, Quantity = 10},
+            new Item {Id=_nextId++, Name = "Hoodies", ItemQuality = 10, Quantity = 20},
+
+            // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/object-and-collection-initializers
+        };
+
+        public List<Item> GetAll()
+        {
+            return new List<Item>(Data);
+            // copy constructor
+            // Callers should no get a reference to the Data object, but rather get a copy
+        }
+
+        public Item GetById(int id)
+        {
+            return Data.Find(item => item.Id == id);
+        }
+
+        public Item Add(Item newItem)
+        {
+            newItem.Id = _nextId++;
+            Data.Add(newItem);
+            return newItem;
+        }
+        public Item Delete(int id)
+        {
+            Item item = Data.Find(item1 => item1.Id == id);
+            if (item == null) return null;
+            Data.Remove(item);
+            return item;
+        }
+
+        public Item Update(int id, Item updates)
+        {
+            Item item = Data.Find(item1 => item1.Id == id);
+            if (item == null) return null;
+            item.Name = updates.Name;
+            item.ItemQuality = updates.ItemQuality;
+            item.Quantity = updates.Quantity;
+            return item;
+        }
+    }
+
+
+}
+

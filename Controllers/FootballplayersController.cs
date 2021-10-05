6e88fb6e44bad43886b461131874e6455f8f92e8
Peter Albinussen <pete47m0@edu.easj.dk>
2021-10-05 15:16:06 +0200

    Football edition

---
+++ Controllers/FootballplayersController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment4SimpleRESTService.Managers;
+using Opgave1UnitTest;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Assignment4SimpleRESTService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballplayersController : ControllerBase
+    {
+        private readonly FootballManagers _footballManagers = new FootballManagers();
+
+        // GET: api/<FootballplayersController>
+        [HttpGet]
+        public IEnumerable<FootballPlayer> Get()
+        {
+            return _footballManagers.GetAll();
+        }
+
+        // GET api/<FootballplayersController>/5
+        [HttpGet("{id}")]
+        public FootballPlayer Get(int id)
+        {
+            return _footballManagers.GetById(id);
+        }
+
+        // POST api/<FootballplayersController>
+        [HttpPost]
+        public FootballPlayer Post([FromBody] FootballPlayer value)
+        {
+            return _footballManagers.Add(value);
+        }
+
+        // PUT api/<FootballplayersController>/5
+        [HttpPut("{id}")]
+        public FootballPlayer Put(int id, [FromBody] FootballPlayer value)
+        {
+            return _footballManagers.Update(id, value);
+        }
+
+        // DELETE api/<FootballplayersController>/5
+        [HttpDelete("{id}")]
+        public FootballPlayer Delete(int id)
+        {
+            return _footballManagers.Delete(id);
+        }
+    }
+}
---
+++ Managers/FootballManagers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment4SimpleRESTService.Models;
+using Opgave1UnitTest;
+
+namespace Assignment4SimpleRESTService.Managers
+{
+    public class FootballManagers 
+    {
+        private static int _nextId = 1;
+
+        private static readonly List <FootballPlayer> Fbplayer = new List<FootballPlayer>
+        {
+            new FootballPlayer {Id = _nextId++, Name = "Mikkel Andersen", Price = 300, ShirtNumber = 12},
+            new FootballPlayer {Id = _nextId++, Name = "Philip Rasmussen", Price = 500, ShirtNumber = 8},
+            new FootballPlayer {Id = _nextId++, Name = "Morten Kejser", Price = 800, ShirtNumber = 4}
+
+        };
+
+
+
+        public List<FootballPlayer> GetAll()
+        {
+            return new List<FootballPlayer>(Fbplayer);
+            // copy constructor
+            // Callers should no get a reference to the Data object, but rather get a copy
+        }
+
+        public FootballPlayer GetById(int id)
+        {
+            return Fbplayer.Find(fbplayer => fbplayer.Id == id);
+        }
+
+        public FootballPlayer Add(FootballPlayer newplayer)
+        {
+            newplayer.Id = _nextId++;
+            Fbplayer.Add(newplayer);
+            return newplayer;
+        }
+        public FootballPlayer Delete(int id)
+        {
+            FootballPlayer fbplayer = Fbplayer.Find(fbplayer1 => fbplayer1.Id == id);
+            if (fbplayer == null) return null;
+            Fbplayer.Remove(fbplayer);
+            return fbplayer;
+        }
+
+        public FootballPlayer Update(int id, FootballPlayer updates)
+        {
+            FootballPlayer fbplayer = Fbplayer.Find(fbplayer1 => fbplayer1.Id == id);
+            if (fbplayer == null) return null;
+            fbplayer.Name = updates.Name;
+            fbplayer.Price = updates.Price;
+            fbplayer.ShirtNumber = updates.ShirtNumber;
+            return fbplayer;
+        }
+    }
+
+}
